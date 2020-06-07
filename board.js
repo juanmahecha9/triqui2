6f3da186e63ef44348b9e30d15dd67e91372295d
Users <user@Userss-iMac.local>
2020-06-06 20:20:07 -0500

    board.js1

--- board.js
+++ board.js
@@ -7,23 +7,26 @@ let c6 = document.getElementById('casilla6');
 let c7 = document.getElementById('casilla7');
 let c8 = document.getElementById('casilla8');
 let c9 = document.getElementById('casilla9');
-let btn = document.getElementById('btn');
+
+let casillas = [[c1, c2, c3], [c4, c5, c6], [c7, c8, c9]]
+
 
 let board = [
     ['x', 'o', 'x'], ['', 'x', 'x'], ['o', 'o', 'x']
 ]
 
-function establecer() {
+// establecer un valor a cada casilla basado en el valor del board
+function establecer(casillas, board) {
     console.log(board)
-    c1.innerText = board[0][0];
-    c2.innerText = board[0][1];
-    c3.innerText = board[0][2];
-    c4.innerText = board[1][0];
-    c5.innerText = board[1][1];
-    c6.innerText = board[1][2];
-    c7.innerText = board[2][0];
-    c8.innerText = board[2][1];
-    c9.innerText = board[2][2];
+    casillas[0][0].innerText = board[0][0];
+    casillas[0][1].innerText = board[0][1];
+    casillas[0][2].innerText = board[0][2];
+    casillas[1][0].innerText = board[1][0];
+    casillas[1][1].innerText = board[1][1];
+    casillas[1][2].innerText = board[1][2];
+    casillas[2][0].innerText = board[2][0];
+    casillas[2][1].innerText = board[2][1];
+    casillas[2][2].innerText = board[2][2];
 }
 
-establecer();
+establecer(casillas, board);
