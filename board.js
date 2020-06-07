fd6114598fea01b0351aa40ae1bbd8753a8f097f
Users <user@Userss-iMac.local>
2020-06-06 22:13:09 -0500

    juego

--- board.js
+++ board.js
@@ -7,6 +7,7 @@ let c6 = document.getElementById('casilla6');
 let c7 = document.getElementById('casilla7');
 let c8 = document.getElementById('casilla8');
 let c9 = document.getElementById('casilla9');
+let btn = document.getElementById('btn');
 
 let casillas = [[c1, c2, c3], [c4, c5, c6], [c7, c8, c9]]
 
@@ -17,7 +18,7 @@ let board = [
 
 // establecer un valor a cada casilla basado en el valor del board
 function establecer(casillas, board) {
-    console.log(board)
+    //console.log(board)
     casillas[0][0].innerText = board[0][0];
     casillas[0][1].innerText = board[0][1];
     casillas[0][2].innerText = board[0][2];
@@ -31,17 +32,30 @@ function establecer(casillas, board) {
 
 
 let turno = 'O';
+let terminado = false;
 // funcion que realiza a travez de un click el cambio del valor de la casilla, y revisa si la casilla esta vacia
 function interaction(casillas) {
     let dato;
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
             casillas[i][j].onclick = () => {
+                if (terminado) {
+                    return;
+                }
                 dato = casillas[i][j].textContent;
-               if (dato != '') {
+                if (dato != '') {
                     return;
                 }
                 casillas[i][j].innerText = turno;
+                let estado = evaluarJuego(casillas);
+
+                terminado = estado.terminado;
+                if (estado.terminado == true) {
+                    estado.casillas[0].classList.add('win')
+                    estado.casillas[1].classList.add('win')
+                    estado.casillas[2].classList.add('win')
+                }
+
                 if (turno == 'X') {
                     turno = 'O'
                 } else {
@@ -52,7 +66,51 @@ function interaction(casillas) {
     }
 }
 
+
+
+function evaluarCasillas(c1, c2, c3) {
+    if (c1.textContent == '') {
+        return;
+    }
+    if (c1.textContent == c2.textContent && c1.textContent == c3.textContent) {
+        return { ganador: c1.textContent, terminado: true, casillas: [c1, c2, c3] }
+    }
+}
+
+// evaluar si las casillas si son iguales
+function evaluarJuego(casillas) {
+    let r;
+    for (let i = 0; i < 3; i++) {
+        r = evaluarCasillas(casillas[i][0], casillas[i][1], casillas[i][2]);
+        if (r) {
+            return r;
+        }
+        r = evaluarCasillas(casillas[0][i], casillas[1][i], casillas[2][i]);
+        if (r) {
+            return r;
+        }
+    }
+    r = evaluarCasillas(casillas[0][0], casillas[1][1], casillas[2][2]);
+    if (r) {
+        return r;
+    }
+    r = evaluarCasillas(casillas[0][2], casillas[1][1], casillas[2][0]);
+    if (r) {
+        return r;
+    }
+
+    return { terminado: false }
+
+}
+
+btn.onclick = () => {
+    establecer(casillas, board);
+}
+
+interaction(casillas);
 establecer(casillas, board);
-interaction(casillas)
+evaluarJuego(board);
+
+
 
 
